a75cbd40139d5493baf408afa514607b3f38c21e
William Woodruff <william@trailofbits.com>
2020-06-29 17:21:45 -0400

    actions/inject_flags: Document

--- src/canker/actions/inject_flags.py
+++ src/canker/actions/inject_flags.py
@@ -12,6 +12,24 @@ logger = logging.getLogger(__name__)
 
 
 class InjectFlags(CompilerAction):
+    """
+    An action for injecting flags into compiler commands (specifically, `cc` and `c++`).
+
+    This action takes `CFLAGS`, `CXXFLAGS`, and `CPPFLAGS` in its configuration and
+    appends them (after shell-splitting) as appropriate.
+
+    For example:
+
+    ```bash
+    CANKER_WRAPPED_CC=clang
+    CANKER_ACTIONS="InjectFlags"
+    CANKER_ACTION_INJECTFLAGS="CFLAGS='-g -O0' CPPFLAGS='-DWHATEVER'"
+    make CC=canker-cc
+    ```
+
+    will cause canker to add `-g -O0 -DWHATEVER` to each `clang` invocation.
+    """
+
     def before_run(self, tool):
         cflags = shlex.split(self._config.get("CFLAGS", ""))
         cxxflags = shlex.split(self._config.get("CXXFLAGS", ""))
