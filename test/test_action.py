9c2f2357ba885a17c1ee878ea7fca5bddac2558d
William Woodruff <william@trailofbits.com>
2021-01-20 14:27:26 -0500

    test_action: make the matrix bigger

--- test/test_action.py
+++ test/test_action.py
@@ -8,13 +8,22 @@ from blight import action, tool
     [
         (action.Action, tool.CC, True),
         (action.CCAction, tool.CC, True),
+        (action.CCAction, tool.CXX, False),
+        (action.CXXAction, tool.CXX, True),
         (action.CXXAction, tool.CC, False),
         (action.CompilerAction, tool.CC, True),
+        (action.CompilerAction, tool.CXX, True),
+        (action.CompilerAction, tool.CPP, False),
         (action.CPPAction, tool.CC, False),
+        (action.CPPAction, tool.CPP, True),
         (action.LDAction, tool.CC, False),
+        (action.LDAction, tool.LD, True),
         (action.ASAction, tool.CC, False),
+        (action.ASAction, tool.AS, True),
         (action.ARAction, tool.CC, False),
+        (action.ARAction, tool.AR, True),
         (action.STRIPAction, tool.CC, False),
+        (action.STRIPAction, tool.STRIP, True),
     ],
 )
 def test_should_run_on(action_class, tool_class, should_run_on):
