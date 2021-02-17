c17483e33e77b7ce35ee14a7031298421506649a
William Woodruff <william@trailofbits.com>
2021-02-17 14:31:44 -0500

    test/actions: add store tests for find_outputs

--- test/actions/test_find_outputs.py
+++ test/actions/test_find_outputs.py
@@ -1,3 +1,4 @@
+import hashlib
 import json
 
 from blight.actions import FindOutputs
@@ -58,6 +59,51 @@ def test_find_outputs_handles_a_out(tmp_path):
         {
             "kind": OutputKind.Executable.value,
             "path": str(cc.cwd / "a.out"),
+            "store_path": None,
+        }
+    ]
+
+
+def test_find_outputs_store(tmp_path):
+    output = tmp_path / "outputs.jsonl"
+    store = tmp_path / "store"
+    contents = b"not a real object file"
+    contents_digest = hashlib.sha256(contents).hexdigest()
+    dummy_foo_o = tmp_path / "foo.o"
+    dummy_foo_o_store = store / f"{dummy_foo_o.name}-{contents_digest}"
+
+    find_outputs = FindOutputs({"output": output, "store": store})
+    cc = CC(["-c", "foo.c", "-o", str(dummy_foo_o)])
+    find_outputs.before_run(cc)
+    # Pretend to be the compiler: write some junk to dummy_foo_o
+    dummy_foo_o.write_bytes(contents)
+    find_outputs.after_run(cc)
+
+    outputs = json.loads(output.read_text())["outputs"]
+    assert outputs == [
+        {
+            "kind": OutputKind.Object.value,
+            "path": str(dummy_foo_o),
+            "store_path": str(dummy_foo_o_store),
+        }
+    ]
+
+
+def test_find_outputs_store_output_does_not_exist(tmp_path):
+    output = tmp_path / "outputs.jsonl"
+    store = tmp_path / "store"
+    dummy_foo_o = tmp_path / "foo.o"
+
+    find_outputs = FindOutputs({"output": output, "store": store})
+    cc = CC(["-c", "foo.c", "-o", str(dummy_foo_o)])
+    find_outputs.before_run(cc)
+    find_outputs.after_run(cc)
+
+    outputs = json.loads(output.read_text())["outputs"]
+    assert outputs == [
+        {
+            "kind": OutputKind.Object.value,
+            "path": str(dummy_foo_o),
             "store_path": None,
         }
     ]
