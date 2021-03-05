5b7c3c7a63462c4539e96ad2505d2caa86bf8cea
sschriner <sonya.schriner@trailofbits.com>
2021-03-04 23:29:00 -0500

    Bitcode Extract Action

---
+++ src/blight/actions/gllvm_bitcode_extract.py
@@ -0,0 +1,30 @@
+"""
+The `GLLVMBitcodeExtract` action.
+"""
+
+import logging
+import os
+
+from blight.action import CompilerAction
+from blight.enums import Lang
+from blight.tool import CompilerTool
+
+logger = logging.getLogger(__name__)
+
+class GLLVMBitcodeExtract(CompilerAction):
+    """
+    Compile and extract bitcode with GLLVM.
+    """
+    def before_run(self, tool: CompilerTool) -> None:
+        if tool.lang in [Lang.C, Lang.Cxx]:
+            # TODO(sonya): these flags are still wrong :|
+            tool.args.extend(["-fwhole-program-vtables", "-flto"])
+            os.environ.update(
+                {
+                    "BLIGHT_WRAPPED_CC": "gclang",
+                    "BLIGHT_WRAPPED_CXX": "gclang++",
+                }
+            )
+        else:
+            logger.debug("not extracting bitcode for an unknown language")
+
