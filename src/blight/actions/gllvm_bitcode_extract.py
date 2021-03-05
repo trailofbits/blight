abd93cbae26404b2cea402c4bc75f3063335871e
sschriner <sonya.schriner@trailofbits.com>
2021-03-05 13:57:06 -0500

    Produces bitcode and executable

--- src/blight/actions/gllvm_bitcode_extract.py
+++ src/blight/actions/gllvm_bitcode_extract.py
@@ -7,7 +7,7 @@ import os
 
 from blight.action import CompilerAction
 from blight.enums import Lang
-from blight.tool import CompilerTool
+from blight.tool import CompilerTool, Tool
 
 logger = logging.getLogger(__name__)
 
@@ -19,13 +19,21 @@ class GLLVMBitcodeExtract(CompilerAction):
 
     def before_run(self, tool: CompilerTool) -> None:  # type: ignore
         if tool.lang in [Lang.C, Lang.Cxx]:
-            # TODO(sonya): emits bitcode only (no binary)
-            tool.args.extend(["-emit-llvm", "-c"])
             os.environ.update(
                 {
                     "BLIGHT_WRAPPED_CC": "gclang",
                     "BLIGHT_WRAPPED_CXX": "gclang++",
+                    # NOTE(sonya): both WLLVM and GLLVM use WLLVM_BC_STORE
+                    "WLLVM_BC_STORE": str(tool.cwd),
                 }
             )
+
+            # Update the environment so WLLVM_BC_STORE is defined
+            tool._env = tool._fixup_env()
         else:
             logger.debug("not extracting bitcode for an unknown language")
+
+    def after_run(self, tool: Tool, *, run_skipped: bool = False):
+        # TODO(sonya): Change name of bitcode file to something besides
+        #  the hash of the path to the original bitcode file
+        pass
