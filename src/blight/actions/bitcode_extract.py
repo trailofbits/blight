5246df37bdfa01d16eba3eccd4b414e32e1b4bfc
sschriner <sonya.schriner@trailofbits.com>
2021-03-08 12:08:48 -0500

    modified bitcode filename

--- src/blight/actions/bitcode_extract.py
+++ src/blight/actions/bitcode_extract.py
@@ -4,6 +4,7 @@ The `BitcodeExtract` action.
 
 import logging
 import subprocess
+from typing import List
 
 from blight.action import CompilerAction
 from blight.enums import Lang
@@ -19,8 +20,11 @@ class BitcodeExtract(CompilerAction):
 
     def before_run(self, tool: CompilerTool) -> None:  # type: ignore
         if tool.lang in [Lang.C, Lang.Cxx]:
-            bc_filename = tool.outputs[0] + ".bc" if tool.outputs else "a.out.bc"
-            args = ["-S", "-emit-llvm", "-o", bc_filename, *tool.inputs]
+            args: List[str]
+            if tool.outputs:
+                args = ["-c", "-emit-llvm", "-o", tool.outputs[0] + ".bc", *tool.inputs]
+            else:
+                args = ["-c", "-emit-llvm", *tool.inputs]
             subprocess.run([tool.wrapped_tool(), *args], env=tool._env)
         else:
             logger.debug("not extracting bitcode for an unknown language")
