c72c9ba09010c83385db40219f708827e872270e
sschriner <sonya.schriner@trailofbits.com>
2021-04-12 23:40:44 -0400

    Bitcode Generation Flags

--- src/blight/actions/bitcode_extract.py
+++ src/blight/actions/bitcode_extract.py
@@ -3,12 +3,13 @@ The `BitcodeExtract` action.
 """
 
 import logging
+import os
 import subprocess
 from typing import List
 
 from blight.action import CompilerAction
 from blight.enums import Lang
-from blight.tool import CompilerTool, Tool
+from blight.tool import CompilerTool
 
 logger = logging.getLogger(__name__)
 
@@ -25,6 +26,12 @@ class BitcodeExtract(CompilerAction):
                 args = ["-c", "-emit-llvm", "-o", tool.outputs[0] + ".bc", *tool.inputs]
             else:
                 args = ["-c", "-emit-llvm", *tool.inputs]
+
+            bitcode_flags = os.getenv("LLVM_BITCODE_GENERATION_FLAGS")
+            if bitcode_flags:
+                args.extend(bitcode_flags.split())
+
+            print(tool.wrapped_tool(), *args)
             subprocess.run([tool.wrapped_tool(), *args], env=tool._env)
         else:
             logger.debug("not extracting bitcode for an unknown language")
