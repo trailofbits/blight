dc0368c381952b86b7571407a2c7227fa19501a2
William Woodruff <william.woodruff@trailofbits.com>
2021-05-28 09:56:04 -0400

    blight: create blight.constants, reorg (#43402)

--- src/blight/actions/find_outputs.py
+++ src/blight/actions/find_outputs.py
@@ -2,7 +2,6 @@
 The `FindOutputs` action.
 """
 
-import enum
 import hashlib
 import logging
 import re
@@ -13,26 +12,12 @@ from typing import List, Optional
 from pydantic import BaseModel
 
 from blight.action import Action
+from blight.constants import OUTPUT_SUFFIX_KIND_MAP, OUTPUT_SUFFIX_PATTERN_MAP
+from blight.enums import OutputKind
 from blight.tool import CC, CXX, LD, Tool
 from blight.util import flock_append
 
 logger = logging.getLogger(__name__)
-
-
-@enum.unique
-class OutputKind(str, enum.Enum):
-    """
-    A collection of common output kinds for build tools.
-
-    This enumeration is not exhaustive.
-    """
-
-    Object: str = "object"
-    SharedLibrary: str = "shared"
-    StaticLibrary: str = "static"
-    Executable: str = "executable"
-    KernelModule: str = "kernel"
-    Unknown: str = "unknown"
 
 
 class Output(BaseModel):
@@ -82,41 +67,6 @@ class OutputsRecord(BaseModel):
     class Config:
         arbitrary_types_allowed = True
         json_encoders = {Tool: lambda t: t.asdict()}
-
-
-OUTPUT_SUFFIX_KIND_MAP = {
-    ".o": OutputKind.Object,
-    ".obj": OutputKind.Object,
-    ".so": OutputKind.SharedLibrary,
-    ".dylib": OutputKind.SharedLibrary,
-    ".dll": OutputKind.SharedLibrary,
-    ".a": OutputKind.StaticLibrary,
-    ".lib": OutputKind.StaticLibrary,
-    "": OutputKind.Executable,
-    ".exe": OutputKind.Executable,
-    ".bin": OutputKind.Executable,
-    ".elf": OutputKind.Executable,
-    ".com": OutputKind.Executable,
-    ".ko": OutputKind.KernelModule,
-    ".sys": OutputKind.KernelModule,
-}
-"""
-A mapping of common output suffixes to their (expected) file kinds.
-
-This mapping is not exhaustive.
-"""
-
-
-OUTPUT_SUFFIX_PATTERN_MAP = {
-    r".*\.so\.\d+\.\d+\.\d+$": OutputKind.SharedLibrary,  # anything with libtool
-    r".*\.so\.\d+\.\d+$": OutputKind.SharedLibrary,  # libssl.so.1.1
-    r".*\.so\.\d+$": OutputKind.SharedLibrary,  # libc.so.6
-}
-"""
-A mapping of common output suffix patterns to their (expected) file kinds.
-
-This mapping is not exhaustive.
-"""
 
 
 class FindOutputs(Action):
