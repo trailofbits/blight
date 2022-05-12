5a4140c179537de1333e3a8a3baaf789ee716d72
Facundo Tuesca <facu@tuesca.com>
2022-05-12 15:56:09 +0200

    Add config option to `InjectFlags` for linker-only flags (#43423)
    * Add Pycharm project folder to .gitignore
    
    * actions/InjectFlags: Add config option for linker flags
    
    This adds the configuration options `CFLAGS_LINKER` and `CXXFLAGS_LINKER`
    to the InjectFlags action. The flags specified in those will only be
    appended if the compiler tool will run the linking stage.
    
    For calls that don't run the linking stage (such as those that include
    the `-c`, `-S`, `-E`, etc options), these flags won't be appended.

--- test/actions/test_inject_flags.py
+++ test/actions/test_inject_flags.py
@@ -26,6 +26,32 @@ def test_inject_flags_cxx():
     assert cxx.args == shlex.split("-fake -flags -more -flags -bar")
 
 
+def test_inject_linker_flags():
+    inject_flags = InjectFlags(
+        {
+            "CFLAGS": "-cc-flags",
+            "CFLAGS_LINKER": "-c-linker-flags",
+            "CXXFLAGS": "-cxx-flags",
+            "CXXFLAGS_LINKER": "-cxx-linker-flags",
+        }
+    )
+
+    cc_nolink = CC(["-c"])
+    cc_link = CC(["-fake", "-flags"])
+    cxx_nolink = CXX(["-c"])
+    cxx_link = CXX(["-fake", "-flags"])
+
+    inject_flags.before_run(cc_nolink)
+    inject_flags.before_run(cc_link)
+    inject_flags.before_run(cxx_nolink)
+    inject_flags.before_run(cxx_link)
+
+    assert cc_nolink.args == shlex.split("-c -cc-flags")
+    assert cc_link.args == shlex.split("-fake -flags -cc-flags -c-linker-flags")
+    assert cxx_nolink.args == shlex.split("-c -cxx-flags")
+    assert cxx_link.args == shlex.split("-fake -flags -cxx-flags -cxx-linker-flags")
+
+
 def test_inject_flags_unknown_lang():
     inject_flags = InjectFlags(
         {"CFLAGS": "-these -are -ignored", "CXXFLAGS": "-so -are -these", "CPPFLAGS": "-and -this"}
