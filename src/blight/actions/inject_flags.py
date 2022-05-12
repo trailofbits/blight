b237829cc41afa24f99dac20cf05f125d1390438
Facundo Tuesca <facundo.tuesca@trailofbits.com>
2022-05-12 15:56:09 +0200

    Add config option to `InjectFlags` for linker-only flags (#43423)
    * Add Pycharm project folder to .gitignore
    
    * actions/InjectFlags: Add config option for linker flags
    
    This adds the configuration options `CFLAGS_LINKER` and `CXXFLAGS_LINKER`
    to the InjectFlags action. The flags specified in those will only be
    appended if the compiler tool will run the linking stage.
    
    For calls that don't run the linking stage (such as those that include
    the `-c`, `-S`, `-E`, etc options), these flags won't be appended.

--- src/blight/actions/inject_flags.py
+++ src/blight/actions/inject_flags.py
@@ -6,7 +6,7 @@ import logging
 import shlex
 
 from blight.action import CompilerAction
-from blight.enums import Lang
+from blight.enums import CompilerStage, Lang
 from blight.tool import CompilerTool
 
 logger = logging.getLogger(__name__)
@@ -16,8 +16,14 @@ class InjectFlags(CompilerAction):
     """
     An action for injecting flags into compiler commands (specifically, `cc` and `c++`).
 
-    This action takes `CFLAGS`, `CXXFLAGS`, and `CPPFLAGS` in its configuration and
-    appends them (after shell-splitting) as appropriate.
+    This action takes the following flags in its configuration and appends them
+    (after shell-splitting) as appropriate:
+    - `CFLAGS`: Flags to append to every C compiler call
+    - `CFLAGS_LINKER`: Flags to append to every C compiler call that runs the linking
+     stage (i.e: no `-c`, `-e`, `-S`, etc. flags present)
+    - `CXXFLAGS`: Same as `CFLAGS` but for C++
+    - `CFLAGS_LINKER`: Same as `CFLAGS_LINKER`, but for C++
+    - `CPPFLAGS`: Flags to append for the preprocessor stage
 
     For example:
 
@@ -37,14 +43,20 @@ class InjectFlags(CompilerAction):
     # is safely specialized for `CompilerTool`.
     def before_run(self, tool: CompilerTool) -> None:  # type: ignore
         cflags = shlex.split(self._config.get("CFLAGS", ""))
+        cflags_linker = shlex.split(self._config.get("CFLAGS_LINKER", ""))
         cxxflags = shlex.split(self._config.get("CXXFLAGS", ""))
+        cxxflags_linker = shlex.split(self._config.get("CXXFLAGS_LINKER", ""))
         cppflags = shlex.split(self._config.get("CPPFLAGS", ""))
 
         if tool.lang == Lang.C:
             tool.args += cflags
             tool.args += cppflags
+            if tool.stage is CompilerStage.AllStages:
+                tool.args += cflags_linker
         elif tool.lang == Lang.Cxx:
             tool.args += cxxflags
             tool.args += cppflags
+            if tool.stage is CompilerStage.AllStages:
+                tool.args += cxxflags_linker
         else:
             logger.debug("not injecting flags for an unknown language")
