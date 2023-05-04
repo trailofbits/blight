b28d2a83e94d8943e74726770595a94708ff7ad5
William Woodruff <william@trailofbits.com>
2023-05-04 13:44:16 -0400

    blight: add a Lint action (#43481)
    * blight: add a Lint action
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    * actions/lint: lintage
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    * test: Lint action tests
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    * action/lint: lintage
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    * actions: lintage
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    * test: lintage
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    * trunk: upgrade, fix mypy
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    * test: lintage
    
    Signed-off-by: William Woodruff <william@trailofbits.com>
    
    ---------
    
    Signed-off-by: William Woodruff <william@trailofbits.com>

--- src/blight/actions/__init__.py
+++ src/blight/actions/__init__.py
@@ -2,15 +2,32 @@
 Actions supported by blight.
 """
 
-from .benchmark import Benchmark  # noqa: F401
-from .cc_for_cxx import CCForCXX  # noqa: F401
-from .demo import Demo  # noqa: F401
-from .embed_bitcode import EmbedBitcode  # noqa: F401
-from .find_inputs import FindInputs  # noqa: F401
-from .find_outputs import FindOutputs  # noqa: F401
-from .ignore_flags import IgnoreFlags  # noqa: F401
-from .ignore_flto import IgnoreFlto  # noqa: F401
-from .ignore_werror import IgnoreWerror  # noqa: F401
-from .inject_flags import InjectFlags  # noqa: F401
-from .record import Record  # noqa: F401
-from .skip_strip import SkipStrip  # noqa: F401
+from .benchmark import Benchmark
+from .cc_for_cxx import CCForCXX
+from .demo import Demo
+from .embed_bitcode import EmbedBitcode
+from .find_inputs import FindInputs
+from .find_outputs import FindOutputs
+from .ignore_flags import IgnoreFlags
+from .ignore_flto import IgnoreFlto
+from .ignore_werror import IgnoreWerror
+from .inject_flags import InjectFlags
+from .lint import Lint
+from .record import Record
+from .skip_strip import SkipStrip
+
+__all__ = [
+    "Benchmark",
+    "CCForCXX",
+    "Demo",
+    "EmbedBitcode",
+    "FindInputs",
+    "FindOutputs",
+    "IgnoreFlags",
+    "IgnoreFlto",
+    "IgnoreWerror",
+    "InjectFlags",
+    "Lint",
+    "Record",
+    "SkipStrip",
+]
