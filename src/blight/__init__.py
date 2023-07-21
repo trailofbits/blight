2d5eac492dcab4e29db1cbff52d76cb3e7aac224
William Woodruff <william@trailofbits.com>
2023-07-21 12:05:41 -0400

    blight: make the doc landing page slightly better
    Signed-off-by: William Woodruff <william@trailofbits.com>

--- src/blight/__init__.py
+++ src/blight/__init__.py
@@ -1,5 +1,10 @@
 """
 `blight`'s public APIs.
+
+Some specific APIs of interest:
+
+* `blight.tool`: APIs for interacting with specific tool models (e.g. `blight.tool.CC`)
+* `blight.action`: Core interfaces for defining actions (instantiated under `blight.actions`)
 """
 
 __version__ = "0.0.53"
